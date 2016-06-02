aef0cf7e5d0f10e7398371b1c1c2b2b4a5ab0ee9
Joel Lacour <lacour.joel@web.de>
2016-06-03 00:00:42 +0200

    created server skeleton

--- src/main/java/server/PocketServer.java
+++ src/main/java/server/PocketServer.java
@@ -2,16 +2,43 @@ package server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 
 import java.time.Instant;
+import java.io.IOException;
+import java.net.*;
 
 public class PocketServer {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
+	private final ServerSocket server;
+	private boolean run;
+	
+	public PocketServer() throws UnknownHostException, IOException {
+		this(8080, 16, InetAddress.getLocalHost());
+	}
+	
+	public PocketServer(int port, int backlog, InetAddress bindAddr)
+			throws IOException {
+		server = new ServerSocket(port, backlog, bindAddr);
+		log.info("Created server on {}:{} with a backlog of {} connections.",
+				bindAddr.getHostAddress(), port, backlog);
+		run = true;
+	}
+	
 	public void start() {
 		log.info("Starting {} at {}", getClass().getSimpleName(), Instant.now());
 	
+		while(run) {
+			try {
+				log.debug("Waiting for connection...");
+				Socket client = server.accept();
+				log.debug("Accepted connection: {}", client);
+			} catch (IOException e) {
+				log.error("Exception while waiting for connection.", e);
+			}
+		}
 	}
 	
 	public void stop() {
--- src/test/java/test/manual/Start.java
+++ src/test/java/test/manual/Start.java
@@ -8,7 +8,7 @@ import server.PocketServer;
  */
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		
 		new PocketServer().start();
 	}
